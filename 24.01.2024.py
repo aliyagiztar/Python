4edf0a7b120523082f185614bf65b6686b3f853b
aliyagiztar <aliyagiz.tar@gmail.com>
2024-01-24 20:23:28 +0300

    Update 24.01.2024.py
    Testing Repository 0.0.2

--- 24.01.2024.py
+++ 24.01.2024.py
@@ -8,3 +8,14 @@ if not_degeri > 49:
     print("Geçti")
 else:
     print("Kaldı")
+
+# Kullanıcıdan bir sayı alıyoruz ve bu sayıyı float türüne dönüştürüyoruz.
+# We are asking for a number from the user and converting this number to float type.
+sayi = float(input("Lütfen bir sayı giriniz (0 ile 100 arasında): "))
+
+# Eğer sayı 0'dan küçükse veya 100'den büyükse, uygun aralıkta bir değer girilmesi gerektiğini söyleyen bir mesaj yazdırıyoruz.
+# If the number is less than 0 or greater than 100, we print a message stating that a value within the appropriate range must be entered.
+if sayi < 0 or sayi > 100:
+    print("Girdiğiniz sayı 0 ile 100 arasında olmalıdır.")
+else:
+    print("Girdiğiniz sayı uygun aralıktadır.")
