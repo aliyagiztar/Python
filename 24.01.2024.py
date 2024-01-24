c67cb8e661684a742bf339dbb3eb4fbe12cf745f
aliyagiztar <aliyagiz.tar@gmail.com>
2024-01-24 21:59:34 +0300

    Update 24.01.2024.py

--- 24.01.2024.py
+++ 24.01.2024.py
@@ -19,3 +19,26 @@ if sayi < 0 or sayi > 100:
     print("Girdiğiniz sayı 0 ile 100 arasında olmalıdır.")
 else:
     print("Girdiğiniz sayı uygun aralıktadır.")
+
+    # Kullanıcıdan çocuk sayısını alıyoruz.
+    # We are asking for the number of children from the user.
+    cocuk_sayisi = int(input("Lütfen çocuk sayısını giriniz: "))
+
+    # Çocuk sayısının negatif olup olmadığını kontrol ediyoruz.
+    # We check if the number of children is negative.
+    if cocuk_sayisi < 0:
+        print("Çocuk sayısı için negatif sayı girilemez.")
+    else:
+        # Eğer çocuk sayısı 3 veya daha az ise her biri için 200 TL hesaplıyoruz.
+        # If the number of children is 3 or less, we calculate 200 TL for each.
+        if cocuk_sayisi <= 3:
+            ek_odeme = cocuk_sayisi * 200
+        # Eğer çocuk sayısı 3'ten fazla ise her biri için 400 TL hesaplıyoruz.
+        # If the number of children is more than 3, we calculate 400 TL for each.
+        else:
+            ek_odeme = cocuk_sayisi * 400
+
+        # Toplam ek ödemeyi yazdırıyoruz.
+        # We print the total additional payment.
+        print("Toplam ek ödeme: {} TL".format(ek_odeme))
+
